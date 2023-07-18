9b6a1bf86bcb26bd2be87d77382507aba84641da
kubis <kubis@192.168.0.101>
2023-07-18 11:43:20 +0200

    jsicss

--- src/main/java/upload/logowanie/KontrolerLogowania.java
+++ src/main/java/upload/logowanie/KontrolerLogowania.java
@@ -38,7 +38,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -110,8 +110,34 @@ public class KontrolerLogowania implements UsługaPrzechowywaniaPlikow {
 
 	@PostMapping("/proces_rejestracji")
 
-	public String ProcesRejestracji(Uzytkownik uzytkownik) {
+	public String procesRejestracji(@ModelAttribute("uzytkownik") Uzytkownik uzytkownik, Model model) {
+		
+		if (uzytkownik.getEmail() == null || uzytkownik.getEmail().isEmpty() || !uzytkownik.getEmail().contains("@") || uzytkownik.getEmail().length() < 6) {
+	        model.addAttribute("komunikat_email", "Wprowadzony email musi zawierac minimum 6 znaków i musi zawierać @!");
+	        return "rejestracja";
+	    }
+		if (uzytkownik.getUsername() == null || uzytkownik.getUsername().isEmpty() || !uzytkownik.getUsername().matches("^[a-zA-Z0-9]+$")) {
+	        model.addAttribute("komunikat_username", "Nazwa uzytkownika musi zawierać mimimum 6 znaków i nie moze zawierać znaków specjalnych!");
+	        return "rejestracja";
+	    }
+		
+		if (uzytkownik.getHaslo() == null || uzytkownik.getHaslo().isEmpty() || uzytkownik.getHaslo().length() < 6
+		        || !uzytkownik.getHaslo().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9!@#$%^&*]).{6,}$")) {
+		    model.addAttribute("komunikat_haslo", "Hasło musi zawierać co najmniej jedną małą literę, jedną dużą literę, oraz znak specjalny lub cyfrę.");
+	        return "rejestracja";
+	    }
+		
+		if (uzytkownik.getImie() == null || uzytkownik.getImie().isEmpty() || uzytkownik.getImie().length() < 2) {
+	        model.addAttribute("komunikat_imie", "Pole imię jest wymagane!");
+	        return "rejestracja";
+	    }
+		
+		if (uzytkownik.getNazwisko() == null || uzytkownik.getNazwisko().isEmpty() || uzytkownik.getNazwisko().length() < 2) {
+	        model.addAttribute("komunikat_nazwisko", "Pole nazwisko jest wymagane!");
 
+	        return "rejestracja";
+	    }
+		
 		BCryptPasswordEncoder EnkoderHasla = new BCryptPasswordEncoder();
 
 		String ZakodowaneHaslo = EnkoderHasla.encode(uzytkownik.getHaslo());
--- src/main/java/upload/logowanie/Uzytkownik.java
+++ src/main/java/upload/logowanie/Uzytkownik.java
@@ -21,7 +21,7 @@ public class Uzytkownik {
 	@Column(name = "username", nullable = false, unique = true, length = 20)
 	private String username;
 
-	@Column(nullable = false, length = 20)
+	@Column(nullable = false, length = 100)
 	private String haslo;
 
 	@Column(name = "imie", nullable = false, length = 15)
